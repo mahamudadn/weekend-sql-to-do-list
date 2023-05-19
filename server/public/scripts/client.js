22086e4010e474d1bba5492ce8cfdf8712547eb4
Mahamud Aden <mahamudadn@gmail.com>
2023-05-19 18:41:04 -0500

    added style to my css

---
+++ server/public/scripts/client.js
@@ -0,0 +1,16 @@
+$(document).ready(onReady);
+
+ // listen for form submit event
+function onReady() {
+    console.log('js')
+    $('#to-do-List').on('click',addToDo )
+
+    
+}
+
+function addToDo(event) {
+    event.preventDefault();
+    console.log('AddToDo Working');
+
+    
+}
