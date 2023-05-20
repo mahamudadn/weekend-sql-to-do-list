c5b10bbba02fef3576329b3f987888cc8843a0ae
Mahamud Aden <mahamudadn@gmail.com>
2023-05-19 19:10:35 -0500

    added required files to my serverside

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,14 @@
-const pg = require('pg');
+// const pg = require('pg');
 
-const pool = new pg.Pool({
-  // name of our database
-  // this will change for every app!
-  database: 'weekend-to-do-app',
-  // where our db is
-  // localhost === on your computer
-  host: 'localhost',
-  // Postgres listens for network connections on port 5432 by default
-  port: 5432
-})
+// const pool = new pg.Pool({
+//   // name of our database
+//   // this will change for every app!
+//   database: 'weekend-to-do-app',
+//   // where our db is
+//   // localhost === on your computer
+//   host: 'localhost',
+//   // Postgres listens for network connections on port 5432 by default
+//   port: 5432
+// })
 
-module.exports = pool;
+// module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('server/public'));
+
+// ROUTES
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+  });
