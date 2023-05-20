55c50f61b8bbe2cba542791dd36ee7b56b747439
Mahamud Aden <mahamudadn@gmail.com>
2023-05-19 21:28:21 -0500

    Added Ajax.Get with Then and Catch

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,7 +4,7 @@ $(document).ready(onReady);
 function onReady() {
     console.log('js')
     $('#add-button').on('click',addToDo )
-
+    getToDoList();
     
 }
 
@@ -17,7 +17,7 @@ let addNew = $('#to-Do').val();
         url:'/listItems',
         data:{task: addNew}
     }).then(function(response){
-        // getToDoList();
+        getToDoList();
         console.log(response)
     }).catch(function(err){
         console.log(err);
@@ -25,3 +25,16 @@ let addNew = $('#to-Do').val();
 
     
 }
+
+   function getToDoList() {
+    console.log('Hello We have something to do');
+    $.ajax({
+        method:'GET',
+        url:'/listItems',
+    }).then(function(response){
+        console.log('AJAX GET Complete')
+    }).catch(function(error){
+        console.log('Stop There Is Error');
+    })
+
+    };
